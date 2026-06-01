bdece57c6da0d9a37a90920bb02f8796327f3158
fasih-td <freakinfasih@gmail.com>
2026-06-01 12:15:54 +0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAeXHtGfYiCr-eufa4d-GNghmxf2TfYFNY",
-  authDomain: "thinkdigitally-helpdesk-8c050.firebaseapp.com",
-  projectId: "thinkdigitally-helpdesk-8c050",
-  storageBucket: "thinkdigitally-helpdesk-8c050.firebasestorage.app",
-  messagingSenderId: "983089542453",
-  appId: "1:983089542453:web:de5205f04144c03f1fc87c",
-  measurementId: "G-EX96PLX85Q"
+// Firebase web app config — must use window.FIREBASE_CONFIG (required by index.html)
+window.FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyAeXHtGfYiCr-eufa4d-GNghmxf2TfYFNY',
+  authDomain: 'thinkdigitally-helpdesk-8c050.firebaseapp.com',
+  projectId: 'thinkdigitally-helpdesk-8c050',
+  storageBucket: 'thinkdigitally-helpdesk-8c050.firebasestorage.app',
+  messagingSenderId: '983089542453',
+  appId: '1:983089542453:web:de5205f04144c03f1fc87c',
 };
